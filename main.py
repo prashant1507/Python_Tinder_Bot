c428e033a886c2aae3938438d40b8d0f88732171
prashant1507 <pjeet.0733@gmail.com>
2020-07-01 09:49:22 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ from tinder_bot import TinderBot
 
 if __name__ == '__main__':
     bot = TinderBot('./chromedriver', 'username', 'password')
+    bot.clean_up()
+    bot.launch_url()
     bot.login()
     bot.auto_swipe()
